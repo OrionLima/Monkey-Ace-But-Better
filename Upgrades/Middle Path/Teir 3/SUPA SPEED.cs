6f9af8471175a07ca40a8ae43d5bcda0926b58ff
Orion <159525795+OrionLima@users.noreply.github.com>
2024-02-14 18:31:09 +0400

    Add files via upload

---
+++ Upgrades/Middle Path/Teir 3/SUPA SPEED.cs
@@ -0,0 +1,33 @@
+﻿using BTD_Mod_Helper.Api.Towers;
+using BTD_Mod_Helper.Extensions;
+using Il2CppAssets.Scripts.Models.Towers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyAceButBetter.Upgrades.Middle_Path.Teir_3
+{
+    internal class SUPA_SPEED : ModUpgrade<BetterMonkeyAce>
+    {
+        public override int Path => MIDDLE;
+
+        public override int Tier => 3;
+
+        public override int Cost => 5069;
+
+        public override string Name => "SUPA SPEED";
+
+        public override string Description => "Makes you go SUPA FAST";
+
+        public override void ApplyUpgrade(TowerModel towerModel)
+        {
+            var attackModel = towerModel.GetAttackModel();
+            var weaponModel = towerModel.GetWeapon();
+            var projectileModel = weaponModel.projectile;
+
+            weaponModel.rate *= 0.3f;
+        }
+    }
+}
