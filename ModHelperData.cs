f67d6d01f85f1a6735dce57a58ea3d42e2bf545f
Orion <159525795+OrionLima@users.noreply.github.com>
2024-02-14 19:47:20 +0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -9,5 +9,5 @@ public static class ModHelperData
     public const string Description = "This is my first ever BTD6 Mod, Called Better Monkey Ace, It has 3 upgrades on all paths, however, the mod is still in alpha and all paths will be finished to Teir 5 and even a paragon. Go to the github page for more info.";
 
     public const string RepoOwner = "OrionLima"; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = "BetterMonkeyAce"; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoName = "Monkey-Ace-But-Better"; // TODO add your repo name here, also in the download url in README.md
 }
