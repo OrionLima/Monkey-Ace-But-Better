fd71969fb12f5e49717da76a2a169fd7a3659e3c
Orion <159525795+OrionLima@users.noreply.github.com>
2024-02-16 18:37:01 +0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace MonkeyAceButBetter;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "40.0";
-    public const string Version = "1.0.0";
+    public const string Version = "1.1.0";
     public const string Name = "Monkey Ace But Better";
 
     public const string Description = "This is my first ever BTD6 Mod, Called Better Monkey Ace, The mod is officialy out of pre-release (alpha/beta). Go to the github page for more info.";
